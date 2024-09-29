2ae3593bf62b765cf7a9a51ff08ef1fa50e0a576
Vedant <borodevedant4@gmail.com>
2024-09-29 16:56:45 +0530

    react-router-dom installation & setup

--- frontend/travel-story/src/App.jsx
+++ frontend/travel-story/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import React from 'react'
+
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/dashboard' exact element={<Home />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/signup' exact element={<Signup/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
