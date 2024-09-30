e41c4801df72b949a5077f4984f14da15ee09b23
Vedant <borodevedant4@gmail.com>
2024-09-30 23:29:14 +0530

    MongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose")
+const config = require ("./config.json")
 const bcrypt = require ("bcrypt");
 const express = require ("express")
 const cors = require ("cors")
 
 const jwt = require("jsonwebtoken")
+
+mongoose.connect(config.connectionString)
 
 const app = express ()
 app.use(express.json())
