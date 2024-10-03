63a184e44ab3fe063626d91b0c39e42dcebc9459
Vedant <borodevedant4@gmail.com>
2024-10-03 23:44:35 +0530

    fix typo

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-    constusctor(statusCode, data, message = "Success"){
+    constructor(statusCode, data, message = "Success"){
         this.statusCode = statusCode
         this.data = data
         this.message = message
