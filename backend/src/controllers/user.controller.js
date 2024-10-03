ad132992733be0d2b8b22ef59fa00c673db7068d
Vedant <borodevedant4@gmail.com>
2024-10-03 23:09:53 +0530

    add register login logout controller and auth middleware

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,135 @@
+import {User} from "../models/user.model.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {ApiError} from "../utils/ApiError.js"
+
+
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+    
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+    
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh token")
+    }
+}
+
+const registerUser = asyncHandler( async(req, res) => {
+    const {email, fullName, password} = req.body
+
+    if(
+        [fullName, email, password].some((field) => field?.trim === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({ email })
+
+    if(existedUser){
+        throw new ApiError(400, "User with email already exists")
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        password
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(200,  createdUser, "user registered succesfully")
+    )
+
+})
+
+const loginUser = asyncHandler( async (req, res) => {
+    const {email, password} = req.body
+
+    if (!email) {
+        throw new ApiError(400, "email is required")
+    }
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+        throw new ApiError(404, "user does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+    
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshtoken"
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken)
+    .cookie("refreshToken", refreshToken)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in succesfully"
+        )
+    )
+
+})
+
+const logoutUser = asyncHandler ( async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:  {
+                refreshToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+        }
+    
+        return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out"))    
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+}
