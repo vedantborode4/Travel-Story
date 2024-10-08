9eb943cfe0233a305ae4d04e2a0995f77847d4f6
Vedant <borodevedant4@gmail.com>
2024-10-09 01:16:44 +0530

    Get User Info API integration

--- frontend/travel-story/src/components/Navbar.jsx
+++ frontend/travel-story/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import logo from "../assets/images/icons8-logo.svg"
+import logo from "../assets/images/icons8-logo.svg" 
+import ProfileInfo from "../components/Cards/ProfileInfo"
 
-const Navbar = () => {
-
+const Navbar = ({userInfo}) => {
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10'>
         <img src={logo} alt="travel story" className='h-9' />
+
+    <ProfileInfo userInfo={userInfo}/>
     </div>
   )
 }
--- frontend/travel-story/src/pages/Home/Home.jsx
+++ frontend/travel-story/src/pages/Home/Home.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
 
 function Home() {
+
+  const navigate = useNavigate()
+
+  const [userInfo, setUserInfo] = useState(null)
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user")
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user)
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear()
+        navigate("/login")
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo()
+  
+    return () => {
+      
+    }
+  }, [])
+  
+
   return (
     <>
-      <Navbar />
+      <Navbar userInfo = {userInfo}/>
     </>
   )
 }
