92f99993a1ec055a48bbf4706a23177e659d491e
Vedant <borodevedant4@gmail.com>
2024-10-17 22:44:47 +0530

    add Function to delete Travel Story image

--- frontend/travel-story/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/travel-story/src/pages/Home/AddEditTravelStory.jsx
@@ -129,7 +129,33 @@ const AddEditTravelStory = ({
         }
     }
 
-    const handleDeleteStoryImg = async () => {}
+    const handleDeleteStoryImg = async () => {
+    
+        const deleteImgRes = await axiosInstance.delete("/delete-image", {
+            params: {
+            imageUrl: storyInfo.imageUrl,
+            },
+        });
+  
+        if (deleteImgRes.data) {
+            const storyId = storyInfo._id;
+            const postData = {
+            title,
+            story,
+            visitedLocation,
+            visitedDate: moment().valueOf(),
+            imageUrl: "", 
+            };
+        
+            const response = await axiosInstance.put(
+            `/edit-story/${storyId}`,
+            postData
+            );
+        
+            setStoryImg(null)
+        }
+  
+    }
 
   return (
     <div className="relative">
